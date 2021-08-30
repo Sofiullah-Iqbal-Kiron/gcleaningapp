f368f46e3f6957773599b6fb5793715e3abd8a7a
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2021-08-30 17:03:04 +0600

    Add more messages & keep count of time spent by program to clean folders.

--- CleaningApp.c
+++ CleaningApp.c
@@ -1,26 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>   // clock_t, clock(), CLOCKS_PER_SEC
+#include <unistd.h> // sleep()
+#include <ctype.h>
 
 #define second 1000
 
 /*
-    Clears temp, %temp%, prefetch, recent. Run tree with pyautogui then it opens CCleaner.
+    Clears temp, %temp%, prefetch, recent. Run tree with pyautogui then open CCleaner.
 */
 
 int main()
 {
-    system("del /s/q C:\\Windows\\Temp");                                         // temp
-    system("del /s/q C:\\Users\\Hp\\AppData\\Local\\Temp");                       // %temp%
-    system("del /s/q C:\\Windows\\Prefetch");                                     // prefetch
-    system("del /q C:\\Users\\Hp\\AppData\\Roaming\\Microsoft\\Windows\\Recent"); // recent
+    // Count time lapse
+    double time_spent = 0.0;
+
+    printf("Cleaning started at temp, temp2, prefetch and recent.\n\n");
+
+    printf("Cleaning temp...\n");
+    {
+        clock_t begin = clock();
+        system("del /s/q C:\\Windows\\Temp"); // temp
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    }
+    printf("\"temp\" cleaned.\n");
+
+    printf("\nCleaning temp2...\n");
+    {
+        clock_t begin = clock();
+        system("del /s/q C:\\Users\\Hp\\AppData\\Local\\Temp"); // %temp%
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    }
+    printf("\"temp2\" cleaned.\n");
+
+    printf("\nCleaning prefetch...\n");
+    {
+        clock_t begin = clock();
+        system("del /s/q C:\\Windows\\Prefetch"); // prefetch
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    }
+    printf("\"prefetch\" cleaned.\n");
+
+    printf("\nCleaning recent...\n");
+    {
+        clock_t begin = clock();
+        system("del /q C:\\Users\\Hp\\AppData\\Roaming\\Microsoft\\Windows\\Recent"); // recent
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    }
+    printf("\"recent\" cleaned.\n");
+
+    printf("\nFolders cleaned in %f seconds.\n", time_spent);
+
+    // Wait 1 second after folders clean and then run tree with tree_runner.exe(Created with pyautogui).
     Sleep(second);
     system("tree_runner.exe");
-    Sleep(3 * second);
     char run_tree[2];
     printf("\nMake sure:\n1. All of background programs and qbittorrent are closed.\n2. All the folder are accessible.\nRuned tree? [Y/N]: ");
     scanf("%c", &run_tree);
-    if (run_tree[0] == 'y' || run_tree[0] == 'Y')
+    if (tolower(run_tree[0]) == 'y')
     {
         system("cls");
         printf("Opening CCleaner...");
@@ -33,4 +75,6 @@ int main()
         Sleep(3 * second);
         exit(0);
     }
+
+    return 0;
 }
